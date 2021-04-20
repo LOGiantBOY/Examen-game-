be157a4ea612b5d4ceac033e0e424f56ad6c6360
LOGiantBOY <54838377+LOGiantBOY@users.noreply.github.com>
2021-04-20 10:37:47 +0200

    add scene changing scripts
    adding main menu, reset butten at the end of the game

---
+++ restart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restart : MonoBehaviour
+{
+   public void restartScene()
+    {   // als je het einde hebt bereikt kan je op restart klikken en het level opnieuw spelen
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
