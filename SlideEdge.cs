70c78feafdd8ec0d8e82ef6031ed5c73bb38d0e3
LOGiantBOY <54838377+LOGiantBOY@users.noreply.github.com>
2021-04-20 10:38:21 +0200

    add last script
    sliding of edges

---
+++ SlideEdge.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideEdge : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {   // een border zodat je van randjes afglijdt
+            collision.gameObject.GetComponent<PlayerMovement>().enabled = false;
+        }
+    }
+}
